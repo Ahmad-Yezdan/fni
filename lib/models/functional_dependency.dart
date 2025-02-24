0567033616716226d345b9332888fc4b09b18b4a
Rana Yezdan <yezdandev@gmail.com>
2025-02-24 15:22:46 +0500

    defined == operator for uniqueness checking

--- lib/models/functional_dependency.dart
+++ lib/models/functional_dependency.dart
@@ -2,12 +2,31 @@ class FunctionalDependency {
   final List<String> determinant;
   final String dependent;
 
-  FunctionalDependency(this.determinant, this.dependent) {
-    if (determinant.isEmpty || dependent.isEmpty) {
-      throw ArgumentError('Determinant and dependent cannot be empty.');
+  FunctionalDependency(List<String> determinant, this.dependent)
+      : determinant = List.from(determinant)..sort() {
+    if (determinant.isEmpty) {
+      throw ArgumentError('Determinant(s) cannot be empty.');
+    }
+    if (dependent.isEmpty) {
+      throw ArgumentError('Dependent cannot be empty.');
     }
   }
 
   @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+    return other is FunctionalDependency &&
+        other.dependent == dependent &&
+        other.determinant.length == determinant.length &&
+        other.determinant.every((attr) => determinant.contains(attr));
+  }
+
+  @override
+  int get hashCode => Object.hash(dependent, determinant.join(','));
+
+  @override
   String toString() => '${determinant.join(", ")} -> $dependent';
 }
+
+
+
