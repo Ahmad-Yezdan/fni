6cc39858950c99d58a10e467a9b36e2110d4fe14
Rana Yezdan <yezdandev@gmail.com>
2025-02-25 17:23:59 +0500

    added sizedbox for refactoring of normalization analysis section of results screen

--- lib/features/results/widgets/list_section.dart
+++ lib/features/results/widgets/list_section.dart
@@ -5,18 +5,28 @@ class ListSection extends StatelessWidget {
   final Set<List<String>>? keys;
   final Set<String>? attributes;
 
-  const ListSection({super.key, required this.title, this.keys, this.attributes});
+  const ListSection(
+      {super.key, required this.title, this.keys, this.attributes});
 
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(title, style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+        Text(title,
+            style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
         const SizedBox(height: 2),
         if (keys != null)
-          Text(keys!.toList().asMap().entries.map((e) => "${e.key + 1}. ${e.value.join(', ')}").join('\n')),
+          Text(keys!
+              .toList()
+              .asMap()
+              .entries
+              .map((e) => "${e.key + 1}. ${e.value.join(', ')}")
+              .join('\n')),
         if (attributes != null) Text(attributes!.join(", ")),
+        const SizedBox(
+          height: 8,
+        ),
       ],
     );
   }
