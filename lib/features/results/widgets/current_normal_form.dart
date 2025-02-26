746e4f2a9fc86a23f9cfd1345a9750987dea90d9
Rana Yezdan <yezdandev@gmail.com>
2025-02-26 16:14:24 +0500

    handeled bcnf case

--- lib/features/results/widgets/current_normal_form.dart
+++ lib/features/results/widgets/current_normal_form.dart
@@ -5,6 +5,7 @@ class CurrentNormalForm extends StatelessWidget {
   final String title, currentNormalForm;
   final String? reason;
   final List<FunctionalDependency> dependencies;
+  final bool forNC;
 
   const CurrentNormalForm({
     super.key,
@@ -12,6 +13,7 @@ class CurrentNormalForm extends StatelessWidget {
     required this.currentNormalForm,
     this.reason,
     required this.dependencies,
+    required this.forNC,
   });
 
   @override
@@ -19,18 +21,25 @@ class CurrentNormalForm extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(title, style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold,overflow: TextOverflow.ellipsis)),
+        Text(title,
+            style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                overflow: TextOverflow.ellipsis)),
         const SizedBox(height: 2),
         RichText(
           text: TextSpan(
             style: DefaultTextStyle.of(context).style,
             children: [
               const TextSpan(text: 'The table is in '),
-              TextSpan(text: currentNormalForm, style: const TextStyle(fontWeight: FontWeight.w600)),
-              if (reason == null) const TextSpan(text: '.'),
-                            if (reason != null) TextSpan(text: ' due to following $reason:'),
+              TextSpan(
+                  text: currentNormalForm,
+                  style: const TextStyle(fontWeight: FontWeight.w600)),
+              if (reason == null) TextSpan(text: !forNC ? '.' : ". So there's no need to proceed further."),
+              if (reason != null) TextSpan(text: ' due to following $reason:'),
               if (dependencies.isNotEmpty)
-                ...dependencies.asMap().entries.map((entry) => TextSpan(text: "\n${entry.key + 1}. ${entry.value}")),
+                ...dependencies.asMap().entries.map((entry) =>
+                    TextSpan(text: "\n${entry.key + 1}. ${entry.value}")),
             ],
           ),
         ),
