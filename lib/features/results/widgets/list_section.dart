007a45bfd20f5fdc052a6462ac6ee79caaabccd2
Rana Yezdan <yezdandev@gmail.com>
2025-02-26 15:21:51 +0500

    handeled unnecessary spacing

--- lib/features/results/widgets/list_section.dart
+++ lib/features/results/widgets/list_section.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:fni/models/functional_dependency.dart';
 
 class ListSection extends StatelessWidget {
   final String title;
   final Set<List<String>>? keys;
   final Set<String>? attributes;
+  final List<FunctionalDependency>? dependencies;
 
-  const ListSection(
-      {super.key, required this.title, this.keys, this.attributes});
+  const ListSection({
+    super.key,
+    required this.title,
+    this.keys,
+    this.attributes,
+    this.dependencies,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -14,19 +21,35 @@ class ListSection extends StatelessWidget {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Text(title,
-            style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
-        const SizedBox(height: 2),
-        if (keys != null)
+            style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                overflow: TextOverflow.ellipsis)),
+
+        // Conditionally add SizedBox(height: 2) only if there is content below
+        if ((keys?.isNotEmpty ?? false) ||
+            (attributes?.isNotEmpty ?? false) ||
+            (dependencies?.isNotEmpty ?? false))
+          const SizedBox(height: 2),
+
+        if (keys?.isNotEmpty ?? false)
           Text(keys!
               .toList()
               .asMap()
               .entries
               .map((e) => "${e.key + 1}. ${e.value.join(', ')}")
               .join('\n')),
-        if (attributes != null) Text(attributes!.join(", ")),
-        const SizedBox(
-          height: 8,
-        ),
+
+        if (attributes?.isNotEmpty ?? false) Text(attributes!.join(", ")),
+
+        if (dependencies?.isNotEmpty ?? false)
+          Text(dependencies!
+              .asMap()
+              .entries
+              .map((e) => "${e.key + 1}. ${e.value}")
+              .join('\n')),
+
+        SizedBox(height: dependencies?.isNotEmpty ?? false ? 2 : 8),
       ],
     );
   }
